e27ead99c6ed5a27934751d3763d8678c61cc647
bernardo campos <berposdzn@gmail.com>
2024-05-22 15:01:09 +0100

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'destroying'
+Restaurant.destroy_all
+
+puts 'creating seeds'
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+end
+
+puts 'finished seeding'
